6de61b7e289e834502a44554fa3b49488ec19250
Haolin Wu <haolin@haolin.dev>
2025-07-19 22:19:34 +0800

    Update media url, image management and django admin

--- server/backend/urls.py
+++ server/backend/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = "Haolin's Portfolio"
+admin.site.site_title = "Haolin's Portfolio"
+admin.site.index_title = "Portfolio Administration"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/projects/models.py
+++ server/projects/models.py
@@ -10,6 +10,13 @@ class Skill(models.Model):
 
 class Project(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/projects/', blank=True, null=True)
+    priority = models.IntegerField(default=0)
+    featured = models.BooleanField(default=False)
+    year = models.IntegerField(blank=True, null=True)
+    link = models.URLField(blank=True, null=True)
     skills = models.ManyToManyField(Skill, related_name='projects', blank=True)
 
     def __str__(self):
