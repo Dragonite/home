fc09d935fa91f9aa3d618cecad48d685b9df799c
Haolin Wu <haolin@haolin.dev>
2025-07-19 18:21:41 +0800

    Add Project and skills models

--- server/projects/admin.py
+++ server/projects/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Skill, Project
 
 # Register your models here.
+admin.site.register(Skill)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'get_skills']
+    filter_horizontal = ['skills']
+    
+    def get_skills(self, obj):
+        return ", ".join([skill.name for skill in obj.skills.all()])
+    get_skills.short_description = 'Skills'
--- server/projects/models.py
+++ server/projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    skills = models.ManyToManyField(Skill, related_name='projects', blank=True)
+
+    def __str__(self):
+        return self.name
