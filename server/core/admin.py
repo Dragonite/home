cb925afae90d9228d241bb3e1156aabc93508aad
Haolin Wu <haolin@haolin.dev>
2025-07-23 02:36:27 +0800

    Move admin config to admin.py

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import GlobalConfiguration
+from django.conf import settings
+from django_otp.admin import OTPAdminSite
+
+admin.site.site_header = "Haolin's Portfolio"
+admin.site.site_title = "Haolin's Portfolio"
+admin.site.index_title = "Portfolio Administration"
 
 # Register your models here.
 admin.site.register(GlobalConfiguration)
+
+# Manage OTP 
+if settings.DEBUG == False:
+    admin.site.__class__ = OTPAdminSite
