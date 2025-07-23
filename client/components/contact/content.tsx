4808c7b530d24be07e6421441f9d7f5f3226c54e
Haolin Wu <haolin@haolin.dev>
2025-07-23 17:45:27 +0800

    Add contact form

--- client/components/contact/content.tsx
+++ client/components/contact/content.tsx
@@ -3,21 +3,25 @@
 import Link from "next/link"
 import { Button } from "../ui/button"
 import { useConfig } from "@/contexts/global-config";
+import ContactForm from "./form";
 
 const ContactContent = () => {
   const { config } = useConfig();
   return (
     <div className="flex flex-col space-between h-full">
-      <div className="text-sm text-muted-foreground">
-        <span>Feel free to reach out to me through this form, or through</span>
-        <Button 
-          asChild
-          className="py-0 pl-1 pr-0"
-          variant="link"
-        >
-          <Link href={`mailto:${config && config.email ? config.email : 'haolin@haolin.dev'}`}>email</Link>
-        </Button>
-        <span>.</span>
+      <div>
+        <div className="text-sm text-muted-foreground mb-4">
+          <span>Feel free to reach out to me through this form, or through</span>
+          <Button 
+            asChild
+            className="py-0 pl-1 pr-0"
+            variant="link"
+          >
+            <Link href={`mailto:${config && config.email ? config.email : 'haolin@haolin.dev'}`}>email</Link>
+          </Button>
+          <span>.</span>
+        </div>
+        <ContactForm />
       </div>
       <div className="text-sm text-muted-foreground mt-auto">
         <span>Website designed and built by Haolin Wu. For the source code visit the</span>
---
+++ client/components/contact/form.tsx
@@ -0,0 +1,82 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+import { Textarea } from "../ui/textarea"
+ 
+const formSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email("Please enter a valid email address"),
+  message: z.string().min(1, "Message is required").max(1000, "Message must be less than 500 characters"),
+})
+ 
+function ContactForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="mb-4">
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Haolin" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="mb-4">
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="haolin@haolin.dev" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Let's get a coffee!" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button variant="secondary" type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default ContactForm;
